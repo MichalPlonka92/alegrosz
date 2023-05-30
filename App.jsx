0d78e2e95cf8f079d0849201e0c42ab6d80c8580
Michał Płonka <mplonka1992@gmail.com>
2023-05-30 16:08:35 +0200

    prettier

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+
+function App() {
+    const [products, setProducts] = useState([]);
+
+
+    return (
+        <>
+            <h1>Alegrosz</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>{product.name}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default App;
