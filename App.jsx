39d9163df52f5cac25e3f323fcd2948e422707b3
Michał Płonka <mplonka1992@gmail.com>
2023-05-30 17:08:05 +0200

    edit

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
 import {useEffect, useState} from "react";
 import Grid from "@mui/material/Unstable_Grid2";
-import {Button, Card, CardContent, CardMedia, Typography,} from "@mui/material";
-import CardActions from '@mui/material/CardActions';
-
+import {Button, Card, CardContent, CardMedia, TextField, Typography,} from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import {faker} from "@faker-js/faker";
 
 function App() {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         getProducts().then((data) => setProducts(data));
@@ -18,39 +19,57 @@ function App() {
 
     return (
         <>
-            <Grid container spacing={2}>
+            <Grid container rowSpacing={3} columnSpacing={3}>
                 <Grid xs={12}>
                     <h1>Alegrosz</h1>
                 </Grid>
                 <Grid xs={12}>
-                    {products.map((product) => (
-                        <Card key={product.id} sx={{maxWidth: 345}}>
-                            <CardMedia
-                                sx={{height: 140}}
-                                image="/static/images/cards/contemplative-reptile.jpg"
-                                title="green iguana"
-                            />
-                            <CardContent>
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="div"
-                                >
-                                    {product.name}
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="text.secondary"
-                                >
-                                    {product.description}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">See details</Button>
-                            </CardActions>
-                        </Card>
-                    ))}
+                    <TextField
+                        id="standard-basic"
+                        label="Search Products"
+                        variant="standard"
+                        value={search}
+                        onChange={(event) => setSearch(event.target.value)}
+                    />
                 </Grid>
+
+                {products
+                    .filter((product) =>
+                        `${product.name} ${product.description}`
+                            .toLowerCase()
+                            .includes(search.toLowerCase())
+                    )
+                    .map((product) => (
+                        <Grid key={product.id}>
+                            <Card sx={{maxWidth: 345}}>
+                                <CardMedia
+                                    sx={{height: 140}}
+                                    image={faker.image.urlLoremFlickr({
+                                        category: "technics",
+                                    })}
+                                    title="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography
+                                        gutterBottom
+                                        variant="h5"
+                                        component="div"
+                                    >
+                                        {product.name}
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        color="text.secondary"
+                                    >
+                                        {product.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">See details</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
             </Grid>
         </>
     );
